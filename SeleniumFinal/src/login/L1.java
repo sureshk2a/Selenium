950450abb83434f702ec43cbf7b9894edf33be15
sureshk2a <sureshk2a@users.noreply.github.com>
2018-09-29 12:58:14 +0530

    First Commit

---
+++ SeleniumFinal/src/login/L1.java
@@ -0,0 +1,16 @@
+package login;
+
+public class L1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+		public void start()
+		{
+			System.out.println("Program Running....");
+			System.out.println("This is my first repository..");
+		}
+
+	}
+
+
---
+++ SeleniumFinal/src/login/L2.java
@@ -0,0 +1,12 @@
+package login;
+
+public class L2 extends L1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		L1 driver = new L1();
+		driver.start();
+		
+	}
+
+}
