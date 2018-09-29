08318d9d2b242e3b979b75408c4a6d94bf62ec04
sureshk2a <sureshk2a@users.noreply.github.com>
2018-09-29 15:27:21 +0530

    Got site with driver

--- SeleniumFinal/src/login/L1.java
+++ SeleniumFinal/src/login/L1.java
@@ -9,7 +9,7 @@ public class L1 {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\Chrome Driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.ultimateqa.com/complicated-page/");
+		driver.get("https://phptravels.com/demo/");
 
 	}
 
