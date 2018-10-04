b12bd96eb1916d69baeb642c44ba4428fa78f1c8
xyz <xyz@192.168.2.20>
2018-10-04 02:50:55 -0700

    URL changed

--- SeleniumFinal/src/login/L1.java
+++ SeleniumFinal/src/login/L1.java
@@ -10,7 +10,7 @@ public class L1 {
 		// TODO Auto-generated method stub
 		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\Chrome Driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("https://phptravels.com/demo/");
+		driver.get("https://www.phptravels.net/");
 
 	}
 
