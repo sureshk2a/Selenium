d2ab2d4a1ae063330b63125998ad4592a7b31e92
Zeus Machine <Zeus Machine@DESKTOP-TG114JN>
2018-10-04 21:21:01 +0530

    load time check

--- SeleniumFinal/src/login/L1.java
+++ SeleniumFinal/src/login/L1.java
@@ -8,9 +8,13 @@ public class L1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver", "D:\\Selenium\\Chrome Driver\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "F:\\Selenium\\Chrome Driver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		long loadstart = System.currentTimeMillis();
 		driver.get("https://www.phptravels.net/");
+		long loadstop = System.currentTimeMillis();
+		long totaload = loadstop-loadstart;
+		System.out.println("The site loaded in: " +totaload );
 
 	}
 
